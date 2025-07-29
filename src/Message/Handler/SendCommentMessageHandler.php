03758253948e3988377f102baf45a0797b06a9e0
jgauthi <jgauthi@users.noreply.github.com>
2025-07-29 14:47:45 +0200

    New service: LogFileService

--- src/Message/Handler/SendCommentMessageHandler.php
+++ src/Message/Handler/SendCommentMessageHandler.php
@@ -3,6 +3,7 @@ namespace App\Message\Handler;
 
 use App\Message\SendCommentMessage;
 use App\Repository\CommentRepository;
+use App\Service\LogFileService;
 use App\Service\SendDossierCommentsService;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -10,8 +11,11 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 #[AsMessageHandler]
 class SendCommentMessageHandler
 {
-    public function __construct(private CommentRepository $commentRepository, private SendDossierCommentsService $service)
-    {
+    public function __construct(
+        private CommentRepository $commentRepository,
+        private SendDossierCommentsService $service,
+        private LogFileService $logFileService,
+    ) {
     }
 
     /**
@@ -23,6 +27,7 @@ class SendCommentMessageHandler
 
         if (!empty($comment) && empty($comment->getSent())) {
             $this->service->send($comment);
+            $this->logFileService->log(sprintf('Sent mail comment %d for dossier %d', $comment->getId(), $comment->getDossier()->getId()));
         }
     }
 }
---
+++ src/Service/LogFileService.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class LogFileService
+{
+    private const string DEFAULT_FILE = 'messenger';
+    private string $logDir;
+
+    public function __construct(KernelInterface $kernel)
+    {
+        $this->logDir = $kernel->getLogDir();
+    }
+
+    public function log(string $message, string $name = self::DEFAULT_FILE): void
+    {
+        $now = new \DateTime;
+        $logFile = sprintf('%s/%s-%s.log', $this->logDir, $name, $now->format('Y-m-d'));
+
+        error_log(
+            sprintf('[%s] %s'.PHP_EOL, $now->format('Y-m-d H:i:s'), $message),
+            3,
+            $logFile,
+        );
+    }
+}
